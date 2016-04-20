6475d5932494c2d711168d3b2c67711db663b671
Alec Burgdorf <aburgd@usesrs.noreply.github.com>
2016-04-19 21:09:32 -0500

    fixed a method error

--- DHKE2.rb
+++ DHKE2.rb
@@ -1,11 +1,26 @@
 require 'securerandom'
 require 'prime'
 
+# Defines class and methods for DHKE
+class Diffie
+  # Calculates public key
+  def self.pk(g, m, a)
+    return pk = g ** a % m
+  end
+  # Calculate shared secret
+  def self.ss(pk, m, a)
+    return ss = pk ** a % m
+  end
+end
+
+# Define class and methods to check primality
 class Prime
+  # Checks if base is prime
   def self.check_base(g)
     if prime?(g)
       return g
     else
+      # Generates a random base until one is found to be prime
       while !(prime?(g))
         g = SecureRandom.random_number(100000)
       end
@@ -13,83 +28,71 @@ class Prime
     end
   end
 
-  def self.check_mod(p)
-    if prime?(p)
-      return p
+  def self.check_mod(m)
+    if prime?(m)
+      return m
     else
-      while !(prime?(p))
-        p =SecureRandom.random_number(100000)
+      # Generates a random modulo prime until one is found to be prime
+      while !(prime?(m))
+        m = SecureRandom.random_number(100000)
       end
-    return p
+    return m
     end
   end
 
-  def self.check_secret(s)
-    if prime?(s)
-      return s
+  def self.check_secret(a)
+    if prime?(a)
+      return a
     else
-      while !(prime?(s))
-        p 'Please enter a prime number: '
+      while !(prime?(a))
+        # Asks for another prime if the first is not prime
+        puts 'Please enter a prime number: '
         s = Integer(gets.chomp)
         return s
       end
     end
+  end
 end
 
-class Diffie
-  def self.publicKey(g, m, a)
-    # Use check_prime method as defined in Prime class for $base and $mod
-    Prime.check_prime($base)
-    Prime.check_prime($mod)
-    # Do the math to find the public key
-    publicKey = g ** a % m
-  end
+# Asks user what they want to calculate
+puts 'Do you want to compute a public key, or a shared secret?'
+answer = gets.chomp
 
-  def self.sharedSecret(publicKey, mod, aSecret)
-    # Do the math to find the shared secret
-    sharedSecret = publicKey ** aSecret % $mod
-  end
-
-  # Ask for which number to find
-  p 'Are you computing a public key, or a shared secret?'
-  answer = gets.chomp.downcase
-
-  # Check the answer
-  # Finding a public key
-  if answer.eql? "public key"
-    # Put randomly generated base prime
-    p 'Your shared prime (g) is:'
-    p $base
-    # Put randomly generated modulo prime
-    p 'Your modulo prime (p) is:'
-    p $mod
-    # Ask for secret prime
-    p 'Enter your secret prime (a):'
-    aSecret = Integer(gets.chomp)
-    # Use secret_prime method as defined in Prime class to check primality
-    # of secret prime
-    Prime.secret_prime(aSecret)
-    # Run publicKey method from Prime class
-    publicKey = Diffie.publicKey($base, $mod, aSecret).to_s
-    p 'Share this with contact: ' + publicKey
-
-  # Finding a shared secret
-  elsif answer.eql? "shared secret"
-    # Ask for public key sent
-    p 'Enter the public key (A) sent to you:'
-    publicKey = gets.chomp
-    # Ask for shared modulo prime
-    p 'Enter the modulo prime (p) sent to you:'
-    mod = Integer(gets.chomp)
-    # Ask for secret prime
-    p 'Enter your secret prime (a):'
-    aSecret = Integer(gets.chomp)
-    # Use secret_prime method as defined in Prime class to check primality
-    # of secret prime
-    Prime.secret_prime(aSecret)
-    # Run sharedSecret method from Prime class
-    sharedSecret = Diffie.sharedSecret(publicKey, $mod, aSecret)
-    # Put sharedSecret to compare
-    p 'Compare this with your contact: ' + sharedSecret
-  end
+# Checks the answer
+if answer.eql? "public key"
+  # Generates a base prime
+  puts 'Your shared prime (g):'
+  g = SecureRandom.random_number(1000000)
+  Prime.check_base(g)
+  puts g
+  # Generates a modulo prime
+  puts 'Your second/modulo prime (p):'
+  m = SecureRandom.random_number(1000000)
+  Prime.check_mod(m)
+  puts g
+  # Asks for and checks secret prime
+  puts 'Enter your secret prime (a):'
+  a = Integer(gets.chomp)
+  Prime.check_secret(a)
+  #Performs public key calculation
+  pk = Diffie.pk(g, m, a)
+  puts "Send this to your contact: #{pk}"
+elsif answer.eql? "shared secret"
+  # Asks for public key
+  puts 'Enter the public key from your contact (pk):'
+  pk = Integer(gets.chomp)
+  # Asks for modulo prime used
+  puts 'Enter the second/modulo prime from your contact (p):'
+  m = Integer(gets.chomp)
+  # Asks for secret prime and checks primality
+  puts 'Enter your secret prime (a):'
+  a = Integer(gets.chomp)
+  Prime.check_secret(a)
+  puts a
+  # Performs shared secret calculation
+  ss = Diffie.ss(pk, m, a)
+  puts "Compare this with your contact: #{ss}"
+else
+  # Quits if answer is invalid
+  puts 'Goodbye.'
 end
