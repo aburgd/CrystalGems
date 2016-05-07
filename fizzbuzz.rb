6e9b03b9d9b62706225206b9ef79d23ee6cec96c
Alec Burgdorf <aburgd@usesrs.noreply.github.com>
2016-05-07 17:56:37 +0000

    added FizzBuzz test

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+nums = (1..100).to_a
+
+for n in nums
+  if n % 3 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  elsif n % 7 == 0
+    puts "Woof"
+  else
+    puts n
+  end
+end
